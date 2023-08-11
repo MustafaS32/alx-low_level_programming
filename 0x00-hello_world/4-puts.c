e838a67db74c9697b3dd9d4e53761bebdcc8aca3
Mustafa <musatafamax32@gmail.com>
2023-08-10 19:01:45 -0700

    first c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * description: 'a c program that prints using the puts function'
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
