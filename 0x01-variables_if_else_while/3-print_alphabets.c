7f784ea6f7f90e475bb4ffa42cbb91ab5543259e
Mustafa <musatafamax32@gmail.com>
2023-08-11 23:04:02 -0700

    l and c alphapet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints alphabets in lower and uppercase
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char f = 'a';
+	char F = 'A';
+
+
+	while (f <= 'z');
+{
+	putchar(f);
+	f++;
+	}
+
+	while (F <= A)
+{
+	putchar(F);
+	F++;
+	}
+	putchar('\n');
+
+	return (0);
+}
