247b1b26a21c2781af5bc9832f1caa062b6758e3
Mustafa <musatafamax32@gmail.com>
2023-08-12 00:35:56 -0700

    from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: single digits nums from 0 to 10
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		printf("%i\n", num);
+		num++;
+	}
+		return (0);
+}
