e5fff3f667ed08c086d7c063c125fb9ed3464a19
Mustafa <musatafamax32@gmail.com>
2023-08-11 22:35:23 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,25 @@
 #include<stdio.h>
 
-/***/
+/**
+ * maib - Entry point
+ *
+ * description: alphabet in lowercase
+ *
+ * Return: 0 (success)
+*/
 
 int main(void)
 {
+	char first = 'a';
 
+	while (first <= 'z')
+	{
+		putchar(first);
+		first++;
+	}
 
+	putchar('\n');
+	return (0);
 
 
 }
