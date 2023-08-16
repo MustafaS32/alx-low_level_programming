a67499959448957b4a80d222dda0f5fd38d3d1b6
Mustafa <musatafamax32@gmail.com>
2023-08-16 10:02:40 -0700

    9 multtable

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - add two iteger nums
+ *
+ * @a: input one
+ * @b: input two
+ *
+ * Return: the sum of a and b
+*/
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
