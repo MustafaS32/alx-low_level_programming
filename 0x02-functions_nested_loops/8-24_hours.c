31f7e849eb8f68f1251bc68a22efeba5d8b4421b
Mustafa <musatafamax32@gmail.com>
2023-08-16 07:40:01 -0700

    time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,14 +6,13 @@
 
 void jack_bauer(void)
 {
-	int min = 0;
-	int hr = 0;
+	int min, hr;
 
-	for (hr <= 23; hr++)
+	for (hr = 0; hr <= 23; hr++)
 
 	{
 
-		for (min <= 59; min++)
+		for (min = 0; min <= 59; min++)
 
 		{
 			_putchar((hr / 10) + 48);
@@ -24,4 +23,4 @@ void jack_bauer(void)
 			_putchar('\n');
 		}
 	}
-i}
+}
