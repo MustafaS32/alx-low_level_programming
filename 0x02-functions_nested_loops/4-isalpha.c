1e1fd1c44e3817452cecd8baed160b48e3d11378
Mustafa <musatafamax32@gmail.com>
2023-08-16 06:00:19 -0700

    l or u case

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,17 +2,18 @@
 
 /**
  * _isalpha - checks if character is lower or uppercase
- * @c: takes input from other functions
+ *  @c: takes input from other functions
  *
- * Retuern: 1 if c is lower or uppercase alpha
- * otherwise return is 0
+ * Return: 1 if c is true else 0
 */
 
 int _isalpha(int c)
+
 {
 	if (c >= 98 && c <= 122)
 		return (1);
 	if (c >= 65 && c <= 90)
 		return (1);
+
 	return (0);
 }
