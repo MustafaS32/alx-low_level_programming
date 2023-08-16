5a782fc2acde2e55ad9e2a0b1df6aab098924d9a
Mustafa <musatafamax32@gmail.com>
2023-08-16 09:49:14 -0700

    9 multtable

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+*/
+
+void times_table(void)
+{
+	int num, mult, sum;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(48);
+			for (mult = 1; mult <= 9; mult++)
+			{
+			_putchar(',');
+			_putchar(' ');
+
+			sum = num * mult;
+
+				if (sum <= 9)
+					_putchar(' ');
+				else
+					_putchar((sum / 10) + 48);
+					_putchar((sum % 10) + 48);
+			}
+	_putchar('\n');
+	}
+}
