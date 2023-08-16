71efb7d4126a32c34f33b7286cd2a3585f8f8eb6
Mustafa <musatafamax32@gmail.com>
2023-08-16 06:35:04 -0700

    lastdigit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,9 +13,9 @@ int print_last_digit(int n)
 	int lastdigit;
 
 	if (n < 0)
-		lastdigit = -1 * (n % 10)
+		lastdigit = -1 * (n % 10);
 	else
-		lastdigit = n % 10
+		lastdigit = n % 10;
 
 	_putchar(lastdigit + 48);
 	return (lastdigit);
