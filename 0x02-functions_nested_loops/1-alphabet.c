db353d96c94bed8f70c3e8487e827a0bd311ce1e
Mustafa <musatafamax32@gmail.com>
2023-08-16 04:28:50 -0700

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - using _putchar poto to print alphabet
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
