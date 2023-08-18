a4c0ab5c0256a963e007a2a4322a909f215feac9
Mustafa <musatafamax32@gmail.com>
2023-08-17 18:55:59 -0700

    m

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,19 +11,19 @@ void print_diagonal(int n)
 	int sl, space;
 
 	if (n <= 0)
-
 		_putchar('\n');
 	else
 	{
 		for (sl = 1; sl <= n; sl++)
 		{
 			for (space = 1; space <= sl; space++)
-
+			{
 				_putchar(' ');
 
 			_putchar('/');
 
 			_putchar('\n');
+			}
 		}
 	}
 
