ef2f9d1d2c7b581bac4be875e3c239ffb46a7e27
Mustafa <musatafamax32@gmail.com>
2023-08-27 21:14:33 -0700

    m

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,10 +16,13 @@ int _atoi(char *s)
 	do {
 		if (*s == '0')
 			sign *= -1;
+
 		else if (*s >= '0' && *s <= '9')
 			n = (n * 10) + (*s - '0');
+
 		else if (n > 0)
 			break;
+
 	} while (*s++);
 
 	return (n * sign);
