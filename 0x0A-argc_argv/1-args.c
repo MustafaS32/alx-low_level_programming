fb2c3715963e72dea6975a38224007c0729733f1
Mustafa <musatafamax32@gmail.com>
2023-11-16 21:12:20 -0800

    argv, argc projects

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -  a program that prints the number of arguments passed into it
+ * @argv: array pointers to arguments
+ * @argc: num of arguments
+ *
+ * Return: 0 (success)
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -  a program that prints all arguments it receives
+ * @argv: pointers array to the prog
+ * @argc: num of arguments in program
+ *
+ * Return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	int x = 0;
+
+	while (argc--)
+	{
+		printf("%s\n", argv[x]);
+		x++;
+	}
+	return (0);
+}
